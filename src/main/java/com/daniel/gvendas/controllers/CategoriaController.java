36de2188c626aa43b38208a972167ecf59083fe6
danielVFS <danielv759153@gmail.com>
2021-07-02 19:45:31 -0300

    Categoria: Update

--- src/main/java/com/daniel/gvendas/controllers/CategoriaController.java
+++ src/main/java/com/daniel/gvendas/controllers/CategoriaController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +41,12 @@ public class CategoriaController {
 		Categoria newCategoria = categoriaService.create(categoria);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(newCategoria);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Categoria> update(@PathVariable Long id, @RequestBody Categoria categoria) {
+		Categoria newCategoria = categoriaService.update(id, categoria);
+		
+		return ResponseEntity.ok(newCategoria);
 	}
 }
--- src/main/java/com/daniel/gvendas/services/CategoriaService.java
+++ src/main/java/com/daniel/gvendas/services/CategoriaService.java
@@ -3,7 +3,9 @@ package com.daniel.gvendas.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.daniel.gvendas.entities.Categoria;
@@ -25,5 +27,23 @@ public class CategoriaService {
 	
 	public Categoria create(Categoria categoria) {
 		return categoriaRepository.save(categoria);
+	}
+	
+	public Categoria update(Long id, Categoria categoria) {
+		Categoria newCategory = categoriaExists(id);
+		
+		BeanUtils.copyProperties(categoria, newCategory, "codigo");
+		
+		return categoriaRepository.save(newCategory);
+	}
+
+	private Categoria categoriaExists(Long id) {
+		Optional<Categoria> categoria = findById(id);
+		
+		if(categoria.isEmpty()) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		
+		return categoria.get();
 	}
 }
